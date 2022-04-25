5c869455ab2e1550d3a2394b9f0b39c916ce9d96
theshire <1919532973@qq.com>
2022-04-25 23:49:04 +0800

    go

--- src/components/rank/Right.vue
+++ src/components/rank/Right.vue
@@ -1,42 +1,66 @@
 <template>
   <div class="container">
-     <div class="topBox">
+    <div class="topBox">
       <div class="left">
-        <img src="" alt="" />
-        <h1></h1>
+        <img :src="data.coverImgUrl" alt="" />
       </div>
-      <div class="right"></div>
+      <div class="right">
+        <div>{{ data.name }}</div>
+      </div>
     </div>
-    <div class="bottomBox"></div> 
+    <div class="bottomBox">
+      <ul>
+        <li v-for="(item, index) in data.tracks" :key="index">
+          <div>{{ item.name }}</div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
-import {ref, reactive, onMounted, nextTick} from 'vue'
-import { getRankListDetail } from '@/api/http'
+import { ref, reactive, onMounted, nextTick, watch, toRefs } from "vue";
+import { getRankListDetail } from "@/api/http";
 export default {
-    name: "Right",
-    props:["idVal"],
-    setup(props){
-      let id = ref(props.idVal)
-      //const idVal = props.idVal
-      console.log(id.value, '---', props.idVal)
-        onMounted(async ()=>{
-          nextTick(()=>{
-            id.value= props.idVal
-          })
-          console.log(id.value)
-          //const res = await getRankListDetail(id.value)
-        })
-        return {
-          id
-        }
+  name: "Right",
+  props: ["idVal"],
+  setup(props) {
+    let topData = reactive({
+      data: "",
+    });
+    onMounted(async () => {});
+    async function getData(id) {
+      const { playlist: res } = await getRankListDetail(id);
+      topData.data = res;
+      console.log(topData.data);
     }
-}
+
+    watch(
+      props,
+      (newV, oldV) => {
+        newV.idVal && getData(newV.idVal);
+      },
+      { immediate: true }
+    );
+    return {
+      ...toRefs(topData),
+    };
+  },
+};
 </script>
 <style lang="scss" scoped>
-.topBox {
-  width: 740px;
-  height: 238px;
-  padding: 40px;
+.container {
+  display: block !important;
+  .topBox {
+    width: 740px;
+    height: 238px;
+    padding: 40px;
+    display: flex;
+    .left {
+      img {
+        width: 150px;
+        height: 150px;
+      }
+    }
+  }
 }
 </style>
